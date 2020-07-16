e4258f10ea55730c3a5929b78744ff8e0e5c8773
oumasma <oumasma93@hotmail.fr>
2020-07-16 19:55:02 +0400

    verif_exo1

--- lib/scrap_crypto.rb
+++ lib/scrap_crypto.rb
@@ -9,18 +9,13 @@ page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) #la page
 crypto_symbols = page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol"]')
 crypto_prices = page.xpath('//td[@class="cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"]')
 
-my_hash = {}
+crypto_array = []
 
 for i in 0..(crypto_symbols.length - 1 )
-my_hash[crypto_symbols[i].text] = crypto_prices[i].text
+crypto_array << {crypto_symbols[i].text => crypto_prices[i].text}
 		
 end
 	
-crypto_array = []
-
-my_hash.each do |hash|
-	crypto_array.push(hash)
-end
 print crypto_array
 
 
